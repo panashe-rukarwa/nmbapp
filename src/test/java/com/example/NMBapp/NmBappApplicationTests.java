15d5a394f5a7c1b9fb4715c2c135f4e886bd5894
panashe-rukarwa <48089302+panashe-rukarwa@users.noreply.github.com>
2022-02-01 12:15:47 +0200

    Add files via upload

---
+++ src/test/java/com/example/NMBapp/NmBappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NMBapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NmBappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
